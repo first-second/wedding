f601347b505aa895cb2023e682e0792977364e66
first-second <firstsecondis72@gmail.com>
2024-01-28 00:49:34 +0530

    touch up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 import os
 from PIL import Image
 
@@ -8,21 +8,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 MAX_IMAGE_SIZE = (1024, 1024)  # Adjust the maximum allowed image size as needed
 
-def compress_image(input_path, output_path, quality=100):
-    """
-    Compress an image using Pillow library.
 
-    :param input_path: Input path of the image.
-    :param output_path: Output path for the compressed image.   
-    :param quality: Compression quality (0 to 100, where 100 is the best quality).
-    """
+def compress_image(input_path, output_path, quality=100):
     with Image.open(input_path) as img:
         img.thumbnail(MAX_IMAGE_SIZE)
         img.save(output_path, quality=quality)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -48,6 +44,7 @@ def upload():
             compress_image(filename, filename)
 
     return 'Files uploaded and compressed successfully!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
